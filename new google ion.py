16ba0668443f3716028b1f60324cab8d230aef25
seech <seechinsu@gmail.com>
2017-03-26 20:46:57 -0400

    rebased commit

--- new google ion.py
+++ new google ion.py
@@ -1,37 +1,45 @@
-import math
-
 def answer(h,q):
+    topconverter = (2**h)-1
+    masks = [(2**i)-1 for i in range(1,h+1)]
     count = 0
     q = q
     solution = []
 
-    #print bin(q)
-    #print bin(q<<1)
-    #mask = (q<<1)-1
-    #print bin(mask)
-    #print bin((q & mask)+1)
-
     def clearbit(child, sumofdiff):
-        mask = (child << 1) - 1
+        mask = max([x for x in masks if x < child])
         newchild = (child & mask) + 1
-        #print newchild
+        #print bin(mask)
+        #print bin(child)
         return newchild, (sumofdiff + (child-newchild))
 
     while ((q+1)& q) !=0 and ((q+2) & (q+1)) !=0:
         q,count = clearbit(q,count)
 
-        print q
-        print count
+        #print q
+        #print count
 
     if ((q+1) & q) == 0:
-        solution.append((2 * q) + 1 + count)
+        if q == topconverter:
+            solution.append(-1)
+        else:
+            solution.append((2 * q) + 1 + count)
+        #print 'a'
     elif ((q+2)&(q+1))==0:
         solution.append(q+1+count)
+        #print 'b'
 
     #print ((q+1) & q)
     #print (q+2)&(q+1)
 
-    print solution
-    return None
+    return solution
 
-answer(15,12)
+print answer(5,12)
+print answer(5,19)
+print answer(5,14)
+print answer(5,28)
+
+print answer(3,7)
+print answer(3,3)
+print answer(3,5)
+print answer(3,1)
+
