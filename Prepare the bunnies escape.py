fe8ba5b2767645cb66a1c4ce6a590e0c757425b7
seech <seechinsu@gmail.com>
2017-04-03 21:37:38 -0400

    rebased commit

--- Prepare the bunnies escape.py
+++ Prepare the bunnies escape.py
@@ -1,121 +1,257 @@
-import  cProfile
+import cprofile
+import sys
+import itertools
 
-class Node(object):
-    def __init__(self, identity, x, y):
-        self.neighbours = []
-        self.identity = identity
-        self.coordinates = (x, y)
+class Vertex:
+    def __init__(self, node):
+        self.id = node
+        self.adjacent = {}
+        # Set distance to infinity for all nodes
+        self.distance = sys.maxint
+        # Mark all nodes unvisited
+        self.visited = False
+        # Predecessor
+        self.previous = None
 
-    def addNeighbour(self, node_key):
-        self.neighbours.append(node_key)
+    def add_neighbor(self, neighbor, weight=0):
+        self.adjacent[neighbor] = weight
 
-class Graph(object):
-    def __init__(self, matrix):
-        self.matrix = matrix
-        self.passable_walls = set()
-        self.nodes = {}
+    def get_connections(self):
+        return self.adjacent.keys()
 
-    def create(self):
-        length = len(self.matrix)
-        for row in range(length):
-            for col in range(length):
-                identity = 0
-                if self.matrix[row][col] == 1:
-                    identity = 1
+    def get_id(self):
+        return self.id
 
-                node = Node(identity, row, col)
+    def get_weight(self, neighbor):
+        return self.adjacent[neighbor]
 
-                # Get the neighbours
-                if self._hasNeighbour(row - 1, col): # Is the top cell there?
-                    node.addNeighbour((row - 1, col))
+    def set_distance(self, dist):
+        self.distance = dist
 
-                if self._hasNeighbour(row + 1, col): # Is the bottom cell there?
-                    node.addNeighbour((row + 1, col))
+    def get_distance(self):
+        return self.distance
 
-                if self._hasNeighbour(row, col - 1): # Is the left cell there?
-                    node.addNeighbour((row, col - 1))
+    def set_previous(self, prev):
+        self.previous = prev
 
-                if self._hasNeighbour(row, col + 1): # Is the right cell there?
-                    node.addNeighbour((row, col + 1))
+    def set_visited(self):
+        self.visited = True
 
-                self.nodes[(row, col)] = node
+    def __str__(self):
+        return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
 
-    def getWalls(self):
-        length = len(self.matrix)
-        traversed_walls = 0
-        for row in range(length):
-            for col in range(length):
-                if self.matrix[row][col] == 1:
-                    self.passable_walls.add((row, col))
 
-    def findShortestPath(self, start, end, passable_wall, path=set()):
-        path.add(start)
-        if start == end:
-            return path
+class Graph:
+    def __init__(self):
+        self.vert_dict = {}
+        self.num_vertices = 0
 
-        if start not in self.nodes:
+    def __iter__(self):
+        return iter(self.vert_dict.values())
+
+    def add_vertex(self, node):
+        self.num_vertices = self.num_vertices + 1
+        new_vertex = Vertex(node)
+        self.vert_dict[node] = new_vertex
+        return new_vertex
+
+    def get_vertex(self, n):
+        if n in self.vert_dict:
+            return self.vert_dict[n]
+        else:
             return None
 
-        shortest = None
-        for node in self.nodes[start].neighbours:
-            if node not in path and (self.nodes[node].identity == 0 or self.nodes[node].coordinates == passable_wall):
-                curr_path = set(path)
-                new_path = self.findShortestPath(self.nodes[node].coordinates, end, passable_wall, curr_path)
-                if new_path:
-                    if not shortest or len(new_path) < len(shortest):
-                        shortest = new_path
+    def add_edge(self, frm, to, cost=0):
+        if frm not in self.vert_dict:
+            self.add_vertex(frm)
+        if to not in self.vert_dict:
+            self.add_vertex(to)
 
-        return shortest
+        self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
+        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
 
-    def _hasNeighbour(self, row, col):
-        length = len(self.matrix)
-        if row < 0 or col < 0 or row >= length or col >= length:
-            return False
+    def get_vertices(self):
+        return self.vert_dict.keys()
 
-        return True
+    def set_previous(self, current):
+        self.previous = current
 
-def answer(matrix):
-    graph = Graph(matrix)
+    def get_previous(self, current):
+        return self.previous
 
-    shortest = None
-    matrix_length = len(matrix)
-    start_node = (0, 0)
-    exit_node = (matrix_length - 1, matrix_length - 1)
-    graph.create()
-    graph.getWalls()
-    for wall in graph.passable_walls:
-        curr_path = graph.findShortestPath(start_node, exit_node, wall, path=set())
-        if curr_path is not None:
-            if shortest is None:
-                shortest = len(curr_path)
+
+def shortest(v, path):
+    ''' make shortest path from v.previous'''
+    if v.previous:
+        path.append(v.previous.get_id())
+        shortest(v.previous, path)
+    return
+
+
+import heapq
+
+
+def dijkstra(aGraph, start, target):
+    print '''Dijkstra's shortest path'''
+    # Set the distance for the start node to zero
+    start.set_distance(0)
+
+    # Put tuple pair into the priority queue
+    unvisited_queue = [(v.get_distance(), v) for v in aGraph]
+    heapq.heapify(unvisited_queue)
+
+    while len(unvisited_queue):
+        # Pops a vertex with the smallest distance
+        uv = heapq.heappop(unvisited_queue)
+        current = uv[1]
+        current.set_visited()
+
+        # for next in v.adjacent:
+        for next in current.adjacent:
+            # if visited, skip
+            if next.visited:
+                continue
+            new_dist = current.get_distance() + current.get_weight(next)
+
+            if new_dist < next.get_distance():
+                next.set_distance(new_dist)
+                next.set_previous(current)
+                print 'updated : current = %s next = %s new_dist = %s' \
+                      % (current.get_id(), next.get_id(), next.get_distance())
             else:
-                if len(curr_path) < shortest:
-                    shortest = len(curr_path)
+                print 'not updated : current = %s next = %s new_dist = %s' \
+                      % (current.get_id(), next.get_id(), next.get_distance())
 
-    return shortest
+        # Rebuild heap
+        # 1. Pop every item
+        while len(unvisited_queue):
+            heapq.heappop(unvisited_queue)
+        # 2. Put all vertices not visited into the queue
+        unvisited_queue = [(v.get_distance(), v) for v in aGraph if not v.visited]
+        heapq.heapify(unvisited_queue)
+
+def legalCoord(i,j):
+    if i <= (len(maze)-1) and j <= (len(maze)-1):
+        return True
+    else:
+        return False
+
+def answer(map):
+    g = Graph()
+    counter = 0
+    mazematrix = {}
+    edges = []
+
+    for i in range(len(maze) ** 2):
+        g.add_vertex(i)
+
+    for i in range(len(maze)):
+        mazematrix[i] = {}
+        for j in range(len(maze)):
+            mazematrix[i][j] = counter
+            counter = counter + 1
+
+    for i in range(len(mazematrix)):
+        for j in range(len(mazematrix[i])):
+
+            if legalCoord(i, j) == True:
+
+                # center grid
+                if 0 < i < (len(mazematrix) - 1) and 0 < j < (len(mazematrix) - 1):
+                    # print i, j, "start"
+                    edges.extend([(mazematrix[i][j], mazematrix[i][j + 1], maze[i][j + 1]),
+                                  (mazematrix[i][j], mazematrix[i + 1][j], maze[i + 1][j]),
+                                  (mazematrix[i][j], mazematrix[i][j - 1], maze[i][j - 1]),
+                                  (mazematrix[i][j], mazematrix[i - 1][j], maze[i - 1][j])])
+
+                # top mid edge
+                if i == 0 and j != 0 and j != (len(mazematrix) - 1):
+                    # print i, j, "start"
+                    edges.extend([(mazematrix[i][j], mazematrix[i + 1][j], maze[i + 1][j]),
+                                  (mazematrix[i][j], mazematrix[i][j + 1], maze[i][j + 1]),
+                                  (mazematrix[i][j], mazematrix[i][j - 1], maze[i][j - 1])])
+
+                # bottom mid range
+                if i == (len(mazematrix) - 1) and j != 0 and j != (len(mazematrix) - 1):
+                    # print i, j, "start"
+                    edges.extend([(mazematrix[i][j], mazematrix[i][j + 1], maze[i][j + 1]),
+                                  (mazematrix[i][j], mazematrix[i][j - 1], maze[i][j - 1]),
+                                  (mazematrix[i][j], mazematrix[i - 1][j], maze[i - 1][j])])
+
+                # left mid range
+                if j == 0 and i != 0 and i != (len(mazematrix) - 1):
+                    # print i, j, "start"
+                    edges.extend([(mazematrix[i][j], mazematrix[i - 1][j], maze[i - 1][j]),
+                                  (mazematrix[i][j], mazematrix[i + 1][j], maze[i + 1][j]),
+                                  (mazematrix[i][j], mazematrix[i][j + 1], maze[i][j + 1])])
+
+                # Right mid range
+                if j == (len(mazematrix) - 1) and i != 0 and i != (len(mazematrix) - 1):
+                    # print i, j, "start"
+                    edges.extend([(mazematrix[i][j], mazematrix[i - 1][j], maze[i - 1][j]),
+                                  (mazematrix[i][j], mazematrix[i + 1][j], maze[i + 1][j]),
+                                  (mazematrix[i][j], mazematrix[i][j - 1], maze[i][j - 1])])
+
+                # left top corner
+                if i == 0 and j == 0:
+                    # print i, j, "start"
+                    edges.extend([(mazematrix[i][j], mazematrix[i][j + 1], maze[i][j + 1]),
+                                  (mazematrix[i][j], mazematrix[i + 1][j], maze[i + 1][j])])
+
+                # right bottom corner
+                if i == (len(mazematrix) - 1) and j == (len(mazematrix) - 1):
+                    # print i, j, "start"
+                    edges.extend([(mazematrix[i][j], mazematrix[i][j - 1], maze[i][j - 1]),
+                                  (mazematrix[i][j], mazematrix[i - 1][j], maze[i - 1][j])])
+
+                # right top corner
+                if i == 0 and j == (len(mazematrix) - 1):
+                    # print i, j, "start"
+                    edges.extend([(mazematrix[i][j], mazematrix[i][j - 1], maze[i][j - 1]),
+                                  (mazematrix[i][j], mazematrix[i + 1][j], maze[i + 1][j])])
+
+                # left bottom corner
+                if i == (len(mazematrix) - 1) and j == 0:
+                    # print i, j, "start"
+                    edges.extend([(mazematrix[i][j], mazematrix[i][j + 1], maze[i][j + 1]),
+                                  (mazematrix[i][j], mazematrix[i - 1][j], maze[i - 1][j])])
+            else:
+                print "not in bound"
+
+    for i in edges:
+        g.add_edge(i[0], i[1], i[2])
 
 maze = [[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-          [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-          [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-          [0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-          [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+    dijkstra(g, g.get_vertex(0), g.get_vertex(len(maze)**2-1))
+
+    target = g.get_vertex(len(maze)**2-1)
+    path = [target.get_id()]
+    shortest(target, path)
+    print 'The shortest path : %s' %(path[::-1])
 
 print cProfile.run('answer(maze)')
-print answer(maze)
+
+#print answer(maze)
 
 
